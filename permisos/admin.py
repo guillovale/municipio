444f1b0562e6832268b35a6895bb2fdb7da27601
guillovale <guillovale@yahoo.com>
2019-08-15 11:54:53 -0500

    final

--- permisos/admin.py
+++ permisos/admin.py
@@ -6,4 +6,10 @@ from django.contrib import admin
 
 from .models import Empleado
 
+from .models import Tipo
+
+from .models import Permiso
+
 admin.site.register(Empleado)
+admin.site.register(Tipo)
+admin.site.register(Permiso)
