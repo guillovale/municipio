d7399688cf083187dc2f23122785fa44bb334eba
guillovale <guillovale@yahoo.com>
2019-08-15 11:27:14 -0500

    permisos modelos

--- permisos/models.py
+++ permisos/models.py
@@ -6,14 +6,14 @@ class Tipo(models.Model):
     tipo_ausencia = models.CharField(max_length=100)
 
 class Empleado(models.Model):
-    cedula = models.CharField(max_length=20)
+	cedula = models.CharField(max_length=20)
 	apellido = models.CharField(max_length=100)
 	nombre = models.CharField(max_length=100)
     
-
 class Permiso(models.Model):
-    tipo_permiso = models.ForeignKey(Tipo)
-	empleado = models.ForeignKey(Empleado)
-    fecha_desde = models.DateTimeField('date published')
+	tipo_permiso = models.ForeignKey(Tipo, on_delete=models.CASCADE)
+	empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+	fecha_desde = models.DateTimeField('date published')
 	fecha_hasta = models.DateTimeField('date published')
-    numero_dias = models.IntegerField(default=0)
+	numero_dias = models.IntegerField(default=0)
+
