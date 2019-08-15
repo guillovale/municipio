95e8b7b58fd41d57a12117c818e02b3d2af0c856
guillovale <guillovale@yahoo.com>
2019-08-15 11:18:03 -0500

    permisos

--- permisos/models.py
+++ permisos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Tipo(models.Model):
+    tipo_ausencia = models.CharField(max_length=100)
+
+class Empleado(models.Model):
+    cedula = models.CharField(max_length=20)
+	apellido = models.CharField(max_length=100)
+	nombre = models.CharField(max_length=100)
+    
+
+class Permiso(models.Model):
+    tipo_permiso = models.ForeignKey(Tipo)
+	empleado = models.ForeignKey(Empleado)
+    fecha_desde = models.DateTimeField('date published')
+	fecha_hasta = models.DateTimeField('date published')
+    numero_dias = models.IntegerField(default=0)
